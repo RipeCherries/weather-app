0f08c36fcff32a1ce833d71bb4df6401790b6403
RipeCherries <alexeyegoshin0403@mail.ru>
2023-01-21 21:42:11 +0700

    Add render location when calling the API

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,71 @@
 const link = "http://api.weatherstack.com/current?access_key=e17e90a1e5ef27e7eca6bb7c977ead36"
 
+const selectors = {
+    day: document.querySelector(".location__info-day"),
+    date: document.querySelector(".location__info-date"),
+    place: document.querySelector(".location__info-place-name")   
+}
+
+let store = {
+    country: "Russia",
+    countryCode: "RU",
+    city: "Novosibirsk",
+    localTime: ""
+}
+
+
 const fetchData = async () => {
-    const result = await fetch(`${link}&query=${store.city}`);
-    const data = await result.json();
+    const weatherResult = await fetch(`${link}&query=${store.city}`);
+    const weatherData = await weatherResult.json();
 
     const {
-        current: {
-            temperature,
-            weather_descriptions
-        },
-
         location: {
             country,
             localtime,
             name
         }
-    } = data;
+    } = weatherData;
+
+    const codeResult = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+    const codeData = await codeResult.json();
+
+    const {
+        cca2
+    } = codeData[0];
+
+    store = {
+        ...store,
+        country: country,
+        countryCode: cca2,
+        city: name,
+        localTime: localtime
+    };
+
+    render();
+}
+
+const render = () => {
+    selectors.day.innerHTML = getDayName(store.localTime);
+    selectors.date.innerHTML = getFullData(store.localTime);
+    selectors.place.innerHTML = getPlaceName(store.countryCode, store.city);
+}
+
+const getDayName = (date) => {
+    const dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    return dayNames[new Date(date).getDay()];
+}
+
+const getFullData = (date) => {
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+    const d = new Date(date);
+
+    return d.getDate() + " " + monthNames[d.getMonth()] + " " + d.getFullYear();
+}
+
+const getPlaceName = (countryCode, city) => {
+    return city + ", " + countryCode; 
 }
 
 fetchData();
