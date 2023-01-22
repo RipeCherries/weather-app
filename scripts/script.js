cd75a66f1a01af171c39dd576cbd7bae5ef092fc
RipeCherries <alexeyegoshin0403@mail.ru>
2023-01-22 20:42:37 +0700

    Added a weather render

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,24 @@ const link = "http://api.weatherstack.com/current?access_key=e17e90a1e5ef27e7eca
 const selectors = {
     day: document.querySelector(".location__info-day"),
     date: document.querySelector(".location__info-date"),
-    place: document.querySelector(".location__info-place-name")
+    place: document.querySelector(".location__info-place-name"),
+    weather_img: document.querySelector(".location__weather-icon"),
+    temperature: document.querySelector(".location__weather-temperature"),
+    weather_description: document.querySelector(".location__weather-descriprion")
 }
 
 let store = {
     country: "Russia",
     countryCode: "RU",
     city: "Novosibirsk",
-    localTime: ""
+    localTime: "",
+    temperature: 0,
+    feelslike: 0,
+    description: "",
+    cloudcover: 0,
+    humidity: 0,
+    wind_dir: "",
+    wind_speed: 0
 }
 
 
@@ -19,6 +29,15 @@ const fetchData = async () => {
     const weatherData = await weatherResult.json();
 
     const {
+        current: {
+            cloudcover,
+            feelslike,
+            humidity,
+            temperature,
+            weather_descriptions,
+            wind_dir,
+            wind_speed
+        },
         location: {
             country,
             localtime,
@@ -38,7 +57,14 @@ const fetchData = async () => {
         country: country,
         countryCode: cca2,
         city: name,
-        localTime: localtime
+        localTime: localtime,
+        temperature: temperature,
+        feelslike: feelslike,
+        description: weather_descriptions[0],
+        cloudcover: cloudcover,
+        humidity: humidity,
+        wind_dir: wind_dir,
+        wind_speed: wind_speed
     };
 
     render();
@@ -48,6 +74,10 @@ const render = () => {
     selectors.day.innerHTML = getDayName();
     selectors.date.innerHTML = getFullData();
     selectors.place.innerHTML = getPlaceName();
+
+    selectors.weather_img.src = `assets/icons/${getImage()}`;
+    selectors.temperature.innerHTML = getTemperature();
+    selectors.weather_description.innerHTML = getWeatherDescription();
 }
 
 const getDayName = () => {
@@ -66,6 +96,24 @@ const getFullData = () => {
 
 const getPlaceName = () => {
     return store.city + ", " + store.countryCode; 
+}
+
+const getImage = () => {
+    const value = store.description.toLowerCase();
+
+    if (value.includes("snow")) {
+        return "snowly.svg";
+    } else if (value.includes("sunny")) {
+        return "sunny.svg";
+    }
+}
+
+const getTemperature = () => {
+    return "" + store.temperature + " °C";
+}
+
+const getWeatherDescription = () => {
+    return store.description.split(",")[0];
 }
 
 fetchData();
